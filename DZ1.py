c8e42c2cc304a43949478fb8a05552ed3e5214d3
nekinoko <166892991+nekinoko@users.noreply.github.com>
2024-04-13 09:09:23 +0300

    Add files via upload

---
+++ DZ1.py
@@ -0,0 +1,12 @@
+# Вывести три строки на экран:
+
+print ('1. 朝ごはんが好き')
+print ("2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+print ('3. Это третий вывод String (строки) на экран.')
+
+print ('________________') # пробел для чуть лучшей читаемости консоли
+# Вывести три целых числа на экран:
+
+print("1. Целое число номер один:", 1)
+print("2. Целое число номер два:", 0)
+print("3. Целое число номер три", -1)
